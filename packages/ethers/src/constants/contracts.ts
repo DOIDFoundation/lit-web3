feacc90b7a705e912e047c1eca5956c28a02b5cf
Issac <Issac>
2022-12-16 15:41:49 +0800

    - chg: Add goerli testnet

--- packages/ethers/src/constants/contracts.ts
+++ packages/ethers/src/constants/contracts.ts
@@ -2,15 +2,18 @@
 export const AllContracts: Record<string, Record<string, string>> = {
   MultiCall: {
     '0x1': '0xeefba1e63905ef1d7acba5a8513c70307c1ce441',
-    '0xaa36a7': '0x53c43764255c17bd724f74c4ef150724ac50a3ed'
+    '0xaa36a7': '0x53c43764255c17bd724f74c4ef150724ac50a3ed',
+    '0x5': '0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e'
   },
   Locker: {
     '0x1': '0x8b2aff81fec4e7787aeeb257b5d99626651ee43f',
-    '0xaa36a7': '0x208ec0ef36e94f582841296dca6f6b61d5823fbe'
+    '0xaa36a7': '0x208ec0ef36e94f582841296dca6f6b61d5823fbe',
+    '0x5': '0xf32950cf48c10431b27eff888d23cb31615dfcb4'
   },
   Resolver: {
     '0x1': '0x8b2aff81fec4e7787aeeb257b5d99626651ee43f',
-    '0xaa36a7': '0x208ec0ef36e94f582841296dca6f6b61d5823fbe'
+    '0xaa36a7': '0x208ec0ef36e94f582841296dca6f6b61d5823fbe',
+    '0x5': '0xf32950cf48c10431b27eff888d23cb31615dfcb4'
   }
 }
 
