87213108b66ecbfee8e9009be26b83b1f62e4e9c
Issac <>
2022-12-18 17:50:54 +0800

    - feat: Add drop-menu component

---
+++ packages/dui/src/menu/drop.ts
@@ -0,0 +1,41 @@
+import { customElement, TailwindElement, html, property } from '../shared/TailwindElement'
+import { bridgeStore, StateController } from '@lit-web3/ethers/src/useBridge'
+
+import style from './drop.css'
+@customElement('dui-drop')
+export class DuiDrop extends TailwindElement(style) {
+  bindStore: any = new StateController(this, bridgeStore)
+  @property({ type: Boolean, reflect: true }) show = false
+
+  hide() {
+    this.unlisten()
+    this.emit('change', (this.show = false))
+  }
+
+  private _chk = (e: Event) => {
+    const el = this.$('.doid-drop')
+    const outside = typeof e.composedPath === 'function' && !e.composedPath().includes(el)
+    if (outside && this.show) this.hide()
+  }
+  listen() {
+    window.addEventListener('click', this._chk)
+  }
+  unlisten() {
+    window.removeEventListener('click', this._chk)
+  }
+
+  protected shouldUpdate(props: Map<PropertyKey, unknown>): boolean {
+    if (props.has('show')) {
+      setTimeout(() => (this.show ? this.listen() : this.unlisten()))
+    }
+    return true
+  }
+  disconnectedCallback() {
+    this.unlisten()
+    super.disconnectedCallback()
+  }
+
+  override render() {
+    return html`<div class="doid-drop"><slot></slot></div>`
+  }
+}
