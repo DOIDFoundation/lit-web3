09ee86305de54f6f2218729ec854137ef2994890
Issac <>
2022-12-23 22:33:34 +0800

    - feat: show nav-links as button on mobile view

--- packages/core/src/screen.ts
+++ packages/core/src/screen.ts
@@ -1,4 +1,6 @@
 import { State, property } from '@lit-app/state'
+export { StateController } from '@lit-app/state'
+import emitter from './emitter'
 
 export const breakpoints = {
   sm: 640,
@@ -14,20 +16,29 @@ type Screen = {
   isMobi: boolean
   md: boolean
   ratio: number
+  ts: number
 }
 export const screen: Screen = {
   isMobi: match(breakpoints.lg),
   md: match(breakpoints.md),
-  ratio: window.devicePixelRatio ?? 2
+  ratio: window.devicePixelRatio ?? 2,
+  ts: 1
 }
 
 class ScreenStore extends State {
   @property({ value: screen, type: Object }) screen!: Screen
+  constructor() {
+    super()
+    const ro = new ResizeObserver(() => {
+      emitter.emit('force-request-update')
+    })
+    ro.observe(document.documentElement)
+  }
   get isMobi() {
-    return match(breakpoints.lg)
+    return this.screen.isMobi && this.screen.ts
   }
   get md() {
-    return match(breakpoints.md)
+    return this.screen.md && this.screen.ts
   }
 }
 export const screenStore = new ScreenStore()
