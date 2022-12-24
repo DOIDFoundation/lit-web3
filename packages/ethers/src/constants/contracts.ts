da7feed32c47cf20aa38e33d2382569b86261fa5
AravClayton <116242667+AaravClayton@users.noreply.github.com>
2022-12-24 21:03:27 +0800

    chg: mainnet resolver contract

--- packages/ethers/src/constants/contracts.ts
+++ packages/ethers/src/constants/contracts.ts
@@ -11,7 +11,7 @@ export const AllContracts: Record<string, Record<string, string>> = {
     '0x5': '0xf32950cf48c10431b27eff888d23cb31615dfcb4'
   },
   Resolver: {
-    '0x1': '0x24256dab379962842691eb8bce11f2b5032bf1b6',
+    '0x1': '0xcb9302da98405ecc50b1d6d4f9671f05e143b5f7',
     '0xaa36a7': '0x24256dab379962842691eb8bce11f2b5032bf1b6',
     '0x5': '0xab4d8acb8538e7f2b81a8e0db6530bbec96678b5'
   }
