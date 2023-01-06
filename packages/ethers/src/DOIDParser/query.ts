bdbec87c3deb726cd548dd0b245ccf024671be00
Issac <>
2023-01-06 19:53:37 +0800

    - perf: cache ethaddr/meta to sessionStorage

--- packages/ethers/src/DOIDParser/query.ts
+++ packages/ethers/src/DOIDParser/query.ts
@@ -1,13 +1,30 @@
 import { ZERO } from '../utils'
 import { nameInfo } from '../nsResolver'
 import { getAccount, getGraph } from '../useBridge'
+import { useStorage } from '../useStorage'
 import { subgraphQuery } from '@lit-web3/core/src/graph'
 import { bareTLD } from '../nsResolver/checker'
 
 export const reverseDOIDName = async (DOIDName = ''): Promise<Address> => {
-  const account = await getAccount()
-  if (account) return (await nameInfo(DOIDName)).owner ?? ''
-  const api = await getGraph()
-  const { doids = [] } = await subgraphQuery(`{doids(where:{name:"${bareTLD(DOIDName)}"}){coinType address {id}}}`, api)
-  return doids.length ? doids[0].address.id : ZERO
+  let ethAddr = ''
+  // 1. from cache
+  const storage = await useStorage(`doid.eth.${DOIDName}`, sessionStorage, true)
+  ethAddr = await storage.get()
+  if (ethAddr) return ethAddr
+  // 2. from wallet
+  if (!ethAddr) {
+    const account = await getAccount()
+    if (account) ethAddr = (await nameInfo(DOIDName)).owner ?? ''
+  }
+  // 3. from graph
+  if (!ethAddr) {
+    const api = await getGraph()
+    const { doids = [] } = await subgraphQuery(
+      `{doids(where:{name:"${bareTLD(DOIDName)}"}){coinType address {id}}}`,
+      api
+    )
+    ethAddr = doids.length ? doids[0].address.id : ZERO
+  }
+  if (ethAddr) storage.set(ethAddr)
+  return ethAddr
 }
