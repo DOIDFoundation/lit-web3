290739298bc9f6b41c8c81357e7de47b20c00d9c
AravClayton <>
2023-01-12 13:59:49 +0800

    - chg: wrap owner name for collection query

--- apps/art-scan/src/lib/collection.ts
+++ apps/art-scan/src/lib/collection.ts
@@ -1,3 +1,5 @@
+import { wrapTLD } from '@lit-web3/ethers/src/nsResolver/checker'
+
 export const cookToken = (token: Record<string, any>): NFTToken => {
   const { id, tokenURI } = token
   const [address, tokenID] = id ? id.toString().split('-') : [token.contract.id, token.tokenID]
@@ -12,5 +14,10 @@ export const cookToken = (token: Record<string, any>): NFTToken => {
 export const cookColl = (token: Record<string, any>): Coll => {
   const cooked = cookToken(token)
   const { owner: { id: owner = '', doids = [] } = {} } = token
-  return Object.assign(cooked, { owner, doids })
+  return Object.assign(cooked, {
+    owner,
+    doids: doids.map((doid: any) => {
+      return { ...doid, name: wrapTLD(doid.name) }
+    })
+  })
 }
