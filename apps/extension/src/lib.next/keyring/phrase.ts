43680b79d6c65f11c7a9f5b50c563c283faf64c3
ac.doid <116242667+ac-doid@users.noreply.github.com>
2023-04-28 15:48:42 +0800

    getAddress now sync

--- apps/extension/src/lib.next/keyring/phrase.ts
+++ apps/extension/src/lib.next/keyring/phrase.ts
@@ -1,5 +1,5 @@
 import { AptosAccount } from 'aptos'
-import { mnemonicToSeed, validateMnemonic, generateMnemonic } from 'ethereum-cryptography/bip39'
+import { mnemonicToSeedSync, validateMnemonic, generateMnemonic } from 'ethereum-cryptography/bip39'
 import { wordlist } from 'ethereum-cryptography/bip39/wordlists/english'
 import { bytesToHex } from 'ethereum-cryptography/utils'
 
@@ -28,27 +28,27 @@ export const validatePhrase = (phrase = '') => {
   const err = valid ? '' : 'Bad mnemonic'
   return err
 }
-export const phraseMatch = async (phrase = '', target = '') => {
+export const phraseMatch = (phrase = '', target = '') => {
   if (!phrase) return false
   if (target) {
-    let ethAddr = await getAddress(phrase, AddressType.eth)
+    let ethAddr = getAddress(phrase, AddressType.eth)
     return target ? target === String(ethAddr).toLowerCase() : true
   }
   return true
 }
 
-export const getAddress = async (mnemnoic: string, type?: AddressType) => {
+export const getAddress = (mnemnoic: string, type?: AddressType) => {
   const addrs = Object.fromEntries(Object.keys(AddressType).map((key) => [key, ''])) as MultiChainAddresses
   if (type == AddressType.eth || !type) {
     const wallet = Wallet.fromMnemonic(mnemnoic)
-    addrs[AddressType.eth] = await wallet.getAddress()
+    addrs[AddressType.eth] = wallet.address
   }
   if (type == AddressType.aptos || !type) {
     const apt = AptosAccount.fromDerivePath(`m/44'/637'/0'/0'/0'`, mnemnoic)
     addrs[AddressType.aptos] = apt.address().hex()
   }
   if (type == AddressType.solana || !type) {
-    let seed = await mnemonicToSeed(mnemnoic)
+    let seed = mnemonicToSeedSync(mnemnoic)
     const derivedSeed = derivePath(`m/44'/501'/0'/0'`, bytesToHex(seed)).key
     addrs[AddressType.solana] = Keypair.fromSecretKey(
       nacl.sign.keyPair.fromSeed(derivedSeed).secretKey
