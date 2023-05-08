7e6e47c755c561e8cd3b0ccd6589434194ab60b1
AravClayton <>
2023-05-08 18:59:51 +0800

    add cache for chain address list when doid name switch

--- apps/extension/src/middlewares/getDOIDs.ts
+++ apps/extension/src/middlewares/getDOIDs.ts
@@ -8,10 +8,20 @@ export const getDOIDs: BackgroundMiddlware = async ({ req, state }, next) => {
   next()
 }
 
+const doidAddressCache: Record<string, any> = {}
 export const getMultiChainAddress = (type = undefined): BackgroundMiddlware => {
   return async ({ req, state }, next) => {
     if (!req.headers.isInternal) throw new Error(ERR_METHOD_NOT_ALLOWED)
-    const addresses = await (await getKeyring()).getMultiChainAddress(type)
+    let addresses: any
+    const keyring = await getKeyring()
+    let _name: string = req.body?.name ?? (await keyring.selectedDOID).name
+    if (doidAddressCache.hasOwnProperty(_name)) {
+      addresses = doidAddressCache[_name]
+    } else {
+      addresses = await keyring.getMultiChainAddress(type)
+      doidAddressCache[_name] = Object.assign({}, addresses)
+    }
+
     Object.assign(state, { addresses })
     next()
   }
--- apps/extension/src/views/main/index.ts
+++ apps/extension/src/views/main/index.ts
@@ -14,25 +14,29 @@ export class ViewMain extends TailwindElement(style) {
   bindKeyring: any = new StateController(this, uiKeyring)
   @state() curChain = null as any
 
-  get address() {
-    return uiKeyring.selectedDOID?.address
+  get name() {
+    return uiKeyring.selectedDOID?.name
+  }
+  get key() {
+    const { name: chain } = this.curChain ?? {}
+    return [chain, this.name].join('-')
   }
 
   onSwitch(e: CustomEvent) {
     this.curChain = e.detail
   }
-
   connectedCallback() {
     super.connectedCallback()
   }
-
   render() {
     return html`<div class="main">
       <div class="w-full h-full flex items-stretch">
         <network-list class="bg-gray-200 shrink" @switch=${this.onSwitch}></network-list>
         ${keyed(
-          this.curChain?.name,
-          html`<account-list class="grow py-2 px-4" .chain=${this.curChain}>Addresses:</account-list>`
+          this.key,
+          html`<account-list class="grow py-2 px-4" .chain=${this.curChain} .name=${this.name}
+            >Addresses:</account-list
+          >`
         )}
       </div>
     </div>`
