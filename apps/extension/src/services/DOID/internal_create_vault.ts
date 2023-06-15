343e69baabfe9ab5da1a529d83f492ef0a7732e8
AravClayton <>
2023-06-15 14:28:18 +0800

    - chg: goto /main after create new vault

--- apps/extension/src/services/DOID/internal_create_vault.ts
+++ apps/extension/src/services/DOID/internal_create_vault.ts
@@ -1,5 +1,6 @@
 // import backgroundMessenger from '~/lib.next/messenger/background'
 import { getKeyring } from '~/lib.next/keyring'
+import { popupGoto } from '~/middlewares/unlock'
 
 export const internal_create_vault: BackgroundService = {
   method: 'internal_create_vault',
@@ -9,10 +10,13 @@ export const internal_create_vault: BackgroundService = {
     try {
       let res
       const keyringCtrl = await getKeyring()
-      res = await keyringCtrl.createNewVaultAndRestore(pwd, mnemonic)
-      if (doid) res = await keyringCtrl.bindName(doid)
+      res = await keyringCtrl.createNewVaultAndRestore(doid, pwd, mnemonic)
+      // if (doid) res = await keyringCtrl.bindName(doid)
       // await keyringCtrl.setCompletedOnboarding()
       ctx.res.body = res
+      await new Promise(async (_next) => {
+        await popupGoto({ path: `/main` })(ctx, _next)
+      })
     } catch (e) {
       throw e
     }
