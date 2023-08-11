8718787bcdd274a82b004b8a4bae79be56e8ef57
Issac <Issac>
2023-08-11 20:16:44 +0800

    fix: force testnet

--- packages/doids/src/networks.ts
+++ packages/doids/src/networks.ts
@@ -2,7 +2,7 @@ import { DOIDNetworks } from './constants/networks'
 
 const isProd = import.meta.env.MODE === 'production'
 
-export const defaultChainId: DOIDChainId = isProd ? 'mainnet' : 'testnet'
+export const defaultChainId: DOIDChainId = isProd ? 'testnet' : 'testnet'
 export const defaultNetwork: DOIDNetwork = DOIDNetworks[defaultChainId]
 
 export const getDOIDNetwork = (chainId = defaultChainId) => {
