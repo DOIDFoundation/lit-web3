7d9f615dff9cdb9b81acbdbd20f38c01180f081c
Issac <Issac>
2023-09-04 14:13:12 +0800

    chg: DOID chainId

--- packages/chain/src/doid.ts
+++ packages/chain/src/doid.ts
@@ -3,11 +3,21 @@ const name: ChainName = 'doid'
 const symbol = 'DOID'
 
 export const doid: ChainNetworks = [
+  // {
+  //   name,
+  //   title: 'DOID Mainnet',
+  //   abbr: 'Mainnet',
+  //   id: '1',
+  //   rpc: 'https://api.doid.tech',
+  //   rpcWs: 'ws://api.doid.tech/ws',
+  //   scan: 'http://explorer.doid.tech',
+  //   symbol
+  // },
   {
     name,
     title: 'DOID Testnet',
     abbr: 'Testnet',
-    id: 'testnet',
+    id: '2',
     rpc: 'http://54.221.168.235:8556',
     rpcWs: 'ws://54.221.168.235:8557',
     scan: 'http://explorer.testnet.doid.tech',
