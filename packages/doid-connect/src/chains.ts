31b81c3e38f49165193e236692e2d31e90ff0a89
ac <>
2024-01-21 11:39:31 +0000

    doid/connect: v1.0.0 with mainnet

--- packages/doid-connect/src/chains.ts
+++ packages/doid-connect/src/chains.ts
@@ -3,26 +3,28 @@ import { fantomTestnet as ftn } from 'viem/chains'
 
 export { type Chain, defineChain } from 'viem'
 
-// not ready yet
 export const doid: Chain = defineChain({
-  id: 0xdddd,
-  name: 'DOID Testnet',
-  network: 'doid-testnet',
+  id: 0xd01d,
+  name: 'DOID',
+  network: 'doid',
   nativeCurrency: {
     decimals: 18,
     name: 'DOID',
     symbol: 'DOID'
   },
   rpcUrls: {
-    default: { http: ['https://rpc.testnet.doid.tech'] },
-    public: { http: ['https://rpc.testnet.doid.tech'] }
+    default: { http: ['https://rpc.doid.tech'] },
+    public: { http: ['https://rpc.doid.tech'] }
   },
   blockExplorers: {
-    default: { name: 'DOID Testnet Explorer', url: 'https://scan.testnet.doid.tech' }
+    default: { name: 'DOID Explorer', url: 'https://scan.doid.tech' }
   },
   contracts: {
     ensRegistry: {
-      address: '0x6974201EaAEb277888F6a4028d952E6A59F0baD1'
+      address: '0x8b2afF81fec4E7787AeeB257b5D99626651Ee43F'
+    },
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11'
     }
   }
 })
@@ -46,6 +48,9 @@ export const doidTestnet: Chain = defineChain({
   contracts: {
     ensRegistry: {
       address: '0x6974201EaAEb277888F6a4028d952E6A59F0baD1'
+    },
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11'
     }
   }
 })
