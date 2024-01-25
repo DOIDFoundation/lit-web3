b8a30fdf6663c4a4db19ea466a730f7b758d10f8
AravClayton <>
2024-01-25 17:45:23 +0800

    website hide connect

--- apps/website/src/main.ts
+++ apps/website/src/main.ts
@@ -1,6 +1,5 @@
 import { ThemeElement, html, customElement } from '@lit-web3/dui/shared/theme-element'
 import '~/components/footer'
-import '~/components/connect'
 
 @customElement('app-main')
 export class AppMain extends ThemeElement('') {
