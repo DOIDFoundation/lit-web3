ac72dae73869f2ddf44a93dc819e93ba3e72af66
Issac <Issac>
2024-01-26 22:52:16 +0800

    update

--- packages/doid-connect/src/assets/svg/more.ts
+++ packages/doid-connect/src/assets/svg/more.ts
@@ -1,3 +1,3 @@
 import { svg } from 'lit'
 
-export const moreSvg = svg`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#B6B6BE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevrons-right"><path d="m7 17 5-5-5-5"/><path d="m14 17 5-5-5-5"/></svg>`
+export const moreSvg = svg`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#373737" d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,10.5A1.5,1.5 0 0,0 10.5,12A1.5,1.5 0 0,0 12,13.5A1.5,1.5 0 0,0 13.5,12A1.5,1.5 0 0,0 12,10.5M6.5,10.5A1.5,1.5 0 0,0 5,12A1.5,1.5 0 0,0 6.5,13.5A1.5,1.5 0 0,0 8,12A1.5,1.5 0 0,0 6.5,10.5M17.5,10.5A1.5,1.5 0 0,0 16,12A1.5,1.5 0 0,0 17.5,13.5A1.5,1.5 0 0,0 19,12A1.5,1.5 0 0,0 17.5,10.5Z" /></svg>`
